c34e222069558d75f4e02f826fd4377a0684e097
Saikilito <kembert@gmail.com>
2019-12-19 14:25:37 -0400

    Eventos y templates

--- readme.md
+++ readme.md
@@ -1,153 +1,152 @@
-# Enviar y recibir mensajes
+## Manejo de eventos de menú
 
-Lo primero que debemos hacer es dentro de la plataforma de desarrollo de facebook ubicarnos dentro de nuestra aplicación.
+Para el caso de los eventos del menu asignamos varias opciones y cada uno tiene un nombre de payload al cual podemos apuntar para referirnos a ellos, para esto una crearemos una función `handlePostback` donde manejaremos, en este caso, con un `switch` cada una de estas acciones.
 
-En el apartado de configuración de messeger (se encuentra en el sidebar izquierdo) encontraremos la documentación necesaria para conectarnos.
+Para los eventos de menu, cada acción o postback se distingue por un payload, estos los recibimos en nuestro objeto evento como `evento.postback` y ahora tenemos el poder de responder a esto de difernetes maneras.
 
-## Conectar nuestro webhook
+## Generic Template
+En este ejemplo, lo usaremos para mostrar los productos que estamos ofreciendo, nos mostrara una lista vertical con imagenes, información breve y un boton con el cual podríamos seleccionar dicho producto.
 
-En el apartado de configuración de messeger encontraremos un apartado para webhook, al entrar se abrira un modal que nos pedirá la url de llamada, ahí colocaremos la url `https` que nos ha habilitado el ngrok `{PATH}/webhook/`.
+El template generic podemos llamarlo respondiendo con un `messageData` como el siguiente
 
-También nos va a solicitar el coódigo que nosotros le configuramos en nuestro servidor (`botTest_token` en este caso).
-
-Si la configuración es correcta hasta el momento nos va a confirmar. Luego debemor ir unas opciónes más arriba y usar la opción de generar token, luego volvemos a la sección de webhook y suscribimos, para los campos de suscripción mínimos para enviar, recibir mensajes y crear un menu escogemos las opciones `messages`, `messagging_postbacks` y `messaging_options`.
-
-## Recibir mensajes
-
-Un vez configurado y conectados al webhook esta disponible para recibir mensajes mediante `POST` en el cuerpo del request para el atributo `entry[0].messagin`.
-
-## Enviar mensajes
-
-Nuestro bot podra responder mensajes enviando un `POST` a la uri de `https://graph.facebook.com/me/messages/` esta uri debe ir acompañada de access token que nos genero facebook para acceder a ella, el cuerpo de la respuesta debe tener como mínimo la siguiente estructura
-
-```json
-{
-  "recipient": {
-    "id": "senderId"
+```js
+const messageData = {
+  "recipient":{
+    "id":"<PSID>"
   },
-  "message": {
-    "text": "messageText"
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+         "elements":[
+                      {
+                        "title":"<TITLE_TEXT>",
+                        "image_url":"<IMAGE_URL_TO_DISPLAY>",
+                        "subtitle":"<SUBTITLE_TEXT>",
+                        "default_action": {
+                          "type": "web_url",
+                          "url": "<DEFAULT_URL_TO_OPEN>",
+                          "messenger_extensions": "<TRUE | FALSE>",
+                          "webview_height_ratio": "<COMPACT | TALL | FULL>"
+                        },
+                        "buttons":["<BUTTON_OBJECT>, ..."]      
+                      }
+          ]
+      }
+    }
   }
 }
 ```
-# Integración de boton de Empezar
 
-Esto se hace haciendo una peticion con `curl`, para los usuarios de windows es recomendable usar el git bach o una terminal que este configurada para batch, la linea de comando por si puede generar problemas al enviar la petición.
+Donde elements es un array que contiene objetos que representan a cada uno de los elementos que queremos mostrar.
 
-```batch
-X POST -H "Content-Type: application/json" -d '{
-  "get_started": {"payload": "<postback_payload>"}
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=<PAGE_ACCESS_TOKEN>"
 
-```
+## Responder con imagenes
 
-Luego debemos configurar la información que vamos a mostrar en la pantalla
-
-```batch
-curl -X POST -H "Content-Type: application/json" -d '{
-  "greeting": [
-    {
-      "locale":"default",
-      "text":"Hello!" 
-    }, {
-      "locale":"en_US",
-      "text":"Hi {{user_fist_name}}"
-    }
-  ]
-}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=<PAGE_ACCESS_TOKEN>"
-
-```
-
-# Configurar el menú
-
-Para enviar la configuración que nos permite habilitar un menu persistente en nuestro bot enviamos un JSON por curl con la siguiente forma
-
-```JSON
-{
-    "persistent_menu": [
-        {
-            "locale": "default",
-            "composer_input_disabled": false,
-            "call_to_actions": [
-                {
-                    "type": "postback",
-                    "title": "Talk to an agent",
-                    "payload": "CARE_HELP"
-                },
-                {
-                    "type": "postback",
-                    "title": "Outfit suggestions",
-                    "payload": "CURATION"
-                },
-                {
-                    "type": "web_url",
-                    "title": "Shop now",
-                    "url": "https://www.originalcoastclothing.com/",
-                    "webview_height_ratio": "full"
-                }
-            ]
-        }
-    ]
-}
-```
-
-El ```"composer_input_disabled":true ``` es una opción la cual le permitira al usuario poder escribir o no en el chat, esto es perfecto para cuando queremos que el usuario solo interactue con nuestro bot directamente desde el menu persistente.
-
-El ```payload``` Se suele escribir en mayusculas, es recomendable tener un formato determinado para poder identificarlos rápidamente por lo que otra recomendación es colocarle una terminación del tipo ```"payload":"NAME_PAYLOAD"```
-
-El ```"type":"web_url"``` se usa para mostrar páginas web
-
-## Manejo de eventos
-
-En este caso crearemos una nueva función `handleEvent` en nuestro código para hacer esto.
-
-Como primer evento determinaremos si el lo que estamos recibiendo es un mensaje y si es así invocaremos la funcion `handleMessage` la cual se encargara de manejar este tipo de evento en particular.
-
-Dentro de los mensajes pueden venir diferente tipo de información, lo más comun es un formato de texto lo cual nos llegara dentro de nuestro objeto evento como `evento.text`, pero también podríamos recibir información de tipo audio, imagenes, archivos o videos los cuales recibiremos dentro de nuestro objeto evento como `evento.attachments` el cual contendrá un array con todos la información adjunta recibida, para poder manejar correctamente esta información crearemos una funcion `handleAttachments` para poder discriminar cada tipo de información adjunta recibida que no sea texto y además poder tratarla de la forma que deseemos.
-
-### Respuestas rápidas
-En nuestro manejador de mensajes podemos definir una función `defaultMessage` en ella podemos asignar un mensaje por defecto el cual respondera el bot al recibir un texto pero también podemos darle opciónes para escoger esto lo logramos, por ejemplo, enviando el siguiente `messageData`
+Se puede responder con imagenes o gifs usando el `messageData`
 ```js
 const messageData = {
         "recipient":{
-            id:senderId
-        },
-        "message":{
-            text:"Hola soy un Test Bot, y te invito a usar nuestro menu",
-            "quick_replies":[
-                {
-                    "content_type":"text",
-                    "title":"¿Deseas ordenar?",
-                    "payload":"ORDERS_PAYLOAD"
-                },
-                {
-                    "content_type":"text",
-                    "title":"Acerca de",
-                    "payload":"ABOUT_PAYLOAD"
-                }
-            ]
-        }
+            "id":"<senderId>"
+          },
+          "message":{
+            "attachment": {
+              "type": "image",
+              "payload": {
+                       "url": "<URL_IMAGE>Z"
+                    }
+            }    
+          }
     }
 ```
 
-
-### Animación de tipeo
-Una opción intersante y comun es mostrar antes de responder cada mensaje una animación de tipeo, esto lo podemos lograr muy facil, en nuestro caso crearemos una función `senderActions` la cual debemos invocar antes de responder cada mensaje y que tendra un `messageData` de la siguiente forma
+## Responder con un boton para llamar
+Podemos enviar, por ejemplo, un boton de contacto para llamadas, esto permitira que si el usuario se encuentra hablando con nuestro bot desde el celular automaticamente nuestro bot activara la llamada telefónica cuando el usuario presione el boton
 
 ```js
-    const messageData = {
-        "recipient":{
-            "id": senderId
-        },
-        "sender_action":"typing_on"
+const messageData = {
+  "recipient":{
+    "id":"<PSID>"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"What do you want to do next?",
+        "buttons":[
+          {
+            "type":"web_url",
+            "url":"https://www.messenger.com",
+            "title":"Visit Messenger"
+          },
+          {
+            ...
+          },
+          {...}
+        ]
+      }
     }
+  }
+}
 ```
+## Crear un recibo de pago
 
-Dentro de `sender_action` tambien podemos usar las opciones de 
-* `mark_seen` el cual nos permite dejar los mensajes en visto
-* `typing_off` el cual nos permite ignorar los mensajes recibidos
+Con el `messageData`
 
-
+```js
+const messageData = {
+   "recipient":{
+    "id":"<PSID>"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+         "template_type":"receipt",
+  "recipient_name":"<CUSTOMER_NAME>",
+  "order_number":"<ORDER_NUMBER>",
+  "currency":"<CURRENCY_CODE>",
+  "payment_method":"<PAYMENT_METHOD_USED>",        
+  "order_url":"<LINK_TO_ORDER_SUMMARY>",
+  "timestamp":"<ORDER_TIME_AS_POSIX_TIMESTAMP>",         
+  "address":{
+    "street_1":"<SHIPPING_STREET_ADDRESS>",
+    "city":"<SHIPPING_CITY>",
+    "postal_code":"<SHIPPING_POSTAL_CODE>",
+    "state":"<SHIPPING_STATE>",
+    "country":"<SHIPPING_COUNTRY>"
+  },
+  "summary":{
+    "subtotal": <SUBTOTAL_AMOUNT>,
+    "shipping_cost": <SHIPPING_AMOUNT>,
+    "total_tax": <TAX_AMOUNT>,
+    "total_cost": <TOTAL_AMOUNT>
+  },
+  "adjustments":[
+    {
+      "name": "<ADJUSTMENT_NAME>",
+      "amount": <ADJUSTMENT_AMOUNT>
+    },
+    ...
+  ],
+  "elements":[
+    {
+      "title": "<ITEM_NAME>",
+      "subtitle":"<ITEM_DESCRIPTION_OR_DETAILS>",
+      "quantity": <QUANTITY>,
+      "price": <ITEM_PRICE>,
+      "currency": "<CURRENCY_ABBREVIATION",
+      "image_url":"<URL_IMAGE_TO_DISPLAY_FOR_ITEM>"
+    },
+    ...
+  ]     
+      }
+    }
+  }
+}
+```
 
 
 
--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const access_token = "EAAH5rV3Nwf4BAEMRlvppgrZAHBHOPGUQnwzTmtoiTR47Qqx9UVJq676ZBSZC6BNFZC4wZBZCZACZBTpZBKpZCoZB519ansaJ8ldmCh7IJjXRV6LhRyvlzFLtbR2xZAEG20J75KOfbl0dTWv2MiPTcZAwh0oUWVoT1yuxQJoJZC87uZA1PpOfwZDZD";
+
+require('dotenv').config();
+const access_token = process.env.ACCESS_TOKEN;
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
-    res.send('Hi my nigga');
+    res.send('Hi, welcome to TestBot');
 });
 
 app.get('/webhook',(req,res)=>{
@@ -28,7 +30,6 @@ app.post('/webhook', (req,res)=>{
     //Validemos si existe un mensaje dentro del paquete de datos
     if(webhook_event.messaging){
         webhook_event.messaging.forEach(ev =>{
-            //console.log("Evento",ev);
             handleEvent(ev.sender.id, ev);
         });
     }
@@ -45,8 +46,17 @@ function handleEvent(senderId, event){
 
 function handleMessage(senderId, event){
     if(event.text){
-        console.log(event.text)
-        defaultMessage(senderId)
+        /*--responder un texto--*/
+        //defaultMessage(senderId);
+        /*--responder una imagen--*/
+        messageImage(senderId);
+        /*--responder un numero de contacto para llamar--*/
+        //contactSupport(senderId);
+        /*--responder un menu de localizaciones--*/
+        //showLocation(senderId);
+        /*--responder una factura--*/
+        //receipt(senderId);
+
     }else if(event.attachments){
         handleAttachments(senderId, event)
     }
@@ -58,7 +68,7 @@ function defaultMessage(senderId){
             id:senderId
         },
         "message":{
-            text:"Hola soy un Test Bot, y te invito a usar nuestro menu",
+            "text":"Hola soy un Test Bot, y te invito a usar nuestro menu",
             "quick_replies":[
                 {
                     "content_type":"text",
@@ -93,13 +103,11 @@ function handleAttachments(senderId, ev){
         case "file":
             console.log(attachments_type);
         break;
-    }
-}
-
-function handlePostback(senderId, payload){
-    switch(payload){
-        case "GET_STARTED_TESTBOT":
-            console.log(payload)
+        case "location":
+            console.log(attachments_type);
+        break;
+        default:
+            console.log(attachsments_type);
         break;
     }
 }
@@ -113,6 +121,235 @@ function senderActions(senderId){
     }
     callSendApi(messageData);
 }
+
+function handlePostback(senderId, payload){
+    console.log("Payload Main",payload);
+    switch(payload){
+        case "GET_STARTED_TESTBOT":
+        break;
+        case "PRODUCT_PAYLOAD":
+            showPizzas(senderId);
+        break;
+        case "PEPERONI_PAYLOAD":
+            console.log("Flow?");
+            sizePizza(senderId);
+        break;
+        case "BBQ_PAYLOAD":
+            sizePizza(senderId);
+        break;
+    }
+}
+
+function showPizzas(senderId){
+    const messageData = {
+        "recipient":{
+            "id":senderId
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                        {
+                            "title":"Peperoni",
+                            "subtitle":"Con todo el sabor del peperoni",
+                            "image_url":"https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg",
+                            "buttons":[
+                                {
+                                  "type":"postback",
+                                  "title":"Elegir Personal",
+                                  "payload":"PEPPERONI_PERSONAL_PAYLOAD",
+                                },
+                                {
+                                  "type":"postback",
+                                  "title":"Elegir Mediana",
+                                  "payload":"PEPPERONI_MEDIANA_PAYLOAD",
+                                },
+                                {
+                                  "type":"postback",
+                                  "title":"Elegir Familiar",
+                                  "payload":"PEPPERONI_FAMILIAR_PAYLOAD",
+                                }
+                              ]
+                        },
+                        {
+                            "title":"Pollo BBQ",
+                            "subtitle":"Con todo el sabor del BBQ",
+                            "image_url":"https://www.recetin.com/wp-content/uploads/2010/05/bbq_chicken_pizza-514x300.jpg",
+                            "buttons":[
+                                {
+                                  "type":"postback",
+                                  "title":"Elegir Personal",
+                                  "payload":"BBQ_PERSONAL_PAYLOAD",
+                                },
+                                {
+                                  "type":"postback",
+                                  "title":"Elegir Mediana",
+                                  "payload":"BBQ_MEDIANA_PAYLOAD",
+                                },
+                                {
+                                  "type":"postback",
+                                  "title":"Elegir Familiar",
+                                  "payload":"BBQ_FAMILIAR_PAYLOAD",
+                                }
+                              ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+function messageImage(senderId){
+    const messageData = {
+        "recipient":{
+            "id":senderId
+          },
+          "message":{
+            "attachment": {
+              "type": "image",
+              "payload": {
+                       "url": "https://media.giphy.com/media/H3dsXYs4sNW3eyea58/giphy.gif"
+                    }
+            }    
+          }
+    }
+    callSendApi(messageData);
+}
+
+function contactSupport(senderId){
+    const messageData = {
+        "recipient":{
+            "id": senderId
+        },
+        "message":{
+            "attachment":{
+                "type": "template",
+                "payload":{
+                    "template_type":"button",
+                    "text":"Hola! Quieres llamarnos ?",
+                    "buttons":[
+                        {
+                            "type":"phone_number",
+                            "title":"Llamar a un asesor",
+                            "payload":"+584265555555"
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+function showLocation(senderId){
+    const messageData = {
+        "recipient":{
+            "id": senderId
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":[
+                        {
+                        "title":"Sucursal Venezuela",
+                        "image_url":"https://www.criptonoticias.com/wp-content/uploads/2019/09/venezuela-puerta-am%C3%A9rica-latina-750x375.jpg",
+                        "subtitle":"Caracas, Plaza Candelaria",
+                        "buttons":[
+                            {
+                                "title":"Ver en el mapa",
+                                "type":"web_url",
+                                "url":"https://www.google.com/maps/dir//Caracas,+Distrito+Capital,+Venezuela/@10.4806762,-66.9386259,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8c2a58adcd824807:0x93dd2eae0a998483!2m2!1d-66.9036063!2d10.4805937?hl=es-CL",
+                                "webview_height_ratio":"full"
+                            }
+                        ]      
+                      },
+                      {
+                        "title":"Sucursal Venezuela",
+                        "image_url":"https://www.roastbrief.com.mx/wp-content/uploads/2017/03/monumento_cristo_rey_cali_colombia-640x427.jpg",
+                        "subtitle":"Colombia, Cali",
+                        "buttons":[
+                            {
+                                "title":"Ver en el mapa",
+                                "type":"web_url",
+                                "url":"https://www.google.com/maps/dir//Cali,+Valle+del+Cauca,+Colombia/@3.4517317,-76.567005,13z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8e30a6f0cc4bb3f1:0x1f0fb5e952ae6168!2m2!1d-76.5319854!2d3.4516467?hl=es-CL",
+                                "webview_height_ratio":"tall"
+                            }
+                        ]      
+                      }     
+                    ]
+                }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
+function receipt(senderId){
+    const messageData = {
+        "recipient":{
+            "id": senderId
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload": {
+                    "template_type":"receipt",
+                    "recipient_name":"Kembert Nieves",
+                    "order_number":"123456",
+                    "currency":"USD",
+                    "payment_method":"Tranferencia",        
+                    "order_url":"https://www.azordev-dev.web.app/receipt",
+                    "timestamp": "12365465",         
+                    "address":{
+                      "street_1":"Calle Marogueña",
+                      "city":"Caracas",
+                      "postal_code":"2121",
+                      "state":"Distrito Capital",
+                      "country":"Venezuela"
+                    },
+                    "summary":{
+                      "subtotal": 500.00,
+                      "shipping_cost": 20.00,
+                      "total_tax": 5.00,
+                      "total_cost": 525.00
+                    },
+                    "adjustments":[
+                      {
+                        "name": "Descuento porque si",
+                        "amount": 25.00
+                      }
+                    ],
+                    "elements":[
+                      {
+                        "title": "Pizza Peperoni",
+                        "subtitle":"Pizza sabrosa",
+                        "quantity": 1,
+                        "price": 500,
+                        "currency": "USD",
+                        "image_url":"https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg"
+                      },
+                      {
+                        "title": "Bebida",
+                        "subtitle":"Cafesito",
+                        "quantity": 1,
+                        "price": 50,
+                        "currency": "USD",
+                        "image_url":"https://placeralplato.com/files/2016/01/Pizza-con-pepperoni.jpg"
+                      },
+                    ]
+                  }
+            }
+        }
+    }
+    callSendApi(messageData);
+}
+
 
 function callSendApi(response){
     request({
@@ -133,14 +370,7 @@ function callSendApi(response){
     })
 }
 
-function showPizzas(senderId){
-    const messageData = {
-        "recipient":{
-            "id":senderId
-        }
-    }
-}
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Bot run on port 3000");
 });
